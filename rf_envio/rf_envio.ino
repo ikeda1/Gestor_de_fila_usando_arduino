79f262878e88ea24cfc74e841836b0eac4e36f8e
Pedro Henrique Ikeda <64491246+ikeda1@users.noreply.github.com>
2020-09-22 21:46:40 -0300

    Add files via upload

--- rf_envio/rf_envio.ino
+++ rf_envio/rf_envio.ino
@@ -9,10 +9,12 @@ RF24 radio(9, 10); // Cria uma instância utilizando os pinos (CE, CSN)
 const byte address[6] = "00002"; //CRIA UM ENDEREÇO PARA ENVIO DOS
 //DADOS (O TRANSMISSOR E O RECEPTOR DEVEM SER CONFIGURADOS COM O MESMO ENDEREÇO)
 
-const int senhaNormal = 1; // Variável que indica para o receiver que é senha normal
-const int senhaPref = 2; // Variável que indica para o receiver que é senha preferencial
+int senhaNormal; // Variável que indica para o receiver que é senha normal
+int senhaPref; // Variável que indica para o receiver que é senha preferencial
 
-int tempo = 500;
+unsigned long previousMillis_botao1 = 0;
+unsigned long previousMillis_botao2 = 0;
+int intervalo = 500; // Delay padrão usado nos botões
 
 int cont_senha = 1;  // Contador de senhas
 int cont_senha_pref = 501;  // Contador de senhas preferenciais
@@ -20,7 +22,6 @@ int cont_senha_pref = 501;  // Contador de senhas preferenciais
 void setup()
 {
   Serial.begin(9600);
-  // Serial.println("setup");
   pinMode(botao1, INPUT);
   pinMode(botao2, INPUT);
 
@@ -42,66 +43,77 @@ void loop()
   // Envia senhas normais
   if (digitalRead(botao1) == HIGH && digitalRead(botao2) == LOW)
   {
-    Serial.println("Fila Normal");
-    Serial.println("Senha: ");
-    feed(1);
-    size(51);
-
-    if (cont_senha < 10) 
+    unsigned long currentMillis_botao1 = millis();
+    if ((unsigned long)(currentMillis_botao1 - previousMillis_botao1) >= intervalo)
     {
-      Serial.println("0" + String(cont_senha));
+      previousMillis_botao1 = millis();
+      Serial.println("Fila Normal");
+      Serial.println("Senha: ");
+      feed(1);
+      size(51);
+
+      if (cont_senha < 10)
+      {
+        Serial.println("0" + String(cont_senha));
+      }
+
+      else
+      {
+        Serial.println(cont_senha);
+      }
+
+      senhaNormal = cont_senha; // Variável que recebe a mensagem a ser transmitida
+      radio.write(&senhaNormal, sizeof(senhaNormal)); // Envia a mensagem ao receptor
+
+      cont_senha++;
+
+      // Reseta o contador se a senha normal chegar a 500
+      if (cont_senha == 501)
+      {
+        cont_senha = 1;
+      }
+
+      // digitalWrite(13, true); // Pisca o led built-in para mostrar que enviou dados
+      // digitalWrite(13, false);
+
+      size(1);
+      feed(2);
+      cut();
     }
 
-    else
-    {
-      Serial.println(cont_senha);
-    }
-
-    cont_senha++;
-
-    // Reseta o contador se a senha normal chegar a 500
-    if (cont_senha == 500) 
-    {
-      cont_senha = 1;
-    }
-
-    // senhaNormal = 1; // Variável que recebe a mensagem a ser transmitida
-    radio.write(&senhaNormal, sizeof(int)); // Envia a mensagem ao receptor
-
-    // digitalWrite(13, true); // Pisca o led built-in para mostrar que enviou dados
-    // digitalWrite(13, false);
-
-    size(1);
-    feed(2);
-    cut();
-    delay(tempo);
   }
 
   // Envia senhas preferenciais
   if (digitalRead(botao2) == HIGH && digitalRead(botao1) == LOW)
   {
-    Serial.println("Fila Preferencial");
-    Serial.println("Senha: ");
-    feed(1);
-    size(51);
-
-    Serial.println("P " + String(cont_senha_pref));
-    cont_senha_pref++;
-
-    if (cont_senha_pref == 1000)
+    unsigned long currentMillis_botao2 = millis();
+    if ((unsigned long)(currentMillis_botao2 - previousMillis_botao2) >= intervalo)
     {
-      cont_senha_pref = 501;
+      previousMillis_botao2 = millis();
+      Serial.println("Fila Preferencial");
+      Serial.println("Senha: ");
+      feed(1);
+      size(51);
+
+      Serial.println("P " + String(cont_senha_pref));
+
+      senhaPref = cont_senha_pref;
+      radio.write(&senhaPref, sizeof(senhaPref)); // Envia a mensagem ao receptor
+
+      cont_senha_pref++;
+
+      if (cont_senha_pref == 1001)
+      {
+        cont_senha_pref = 501;
+      }
+
+      // digitalWrite(13, true); // Pisca o led built-in para mostrar que enviou dados
+      // digitalWrite(13, false);
+
+      size(1);
+      feed(2);
+      cut();
     }
-
-    radio.write(&senhaPref, sizeof(int)); // Envia a mensagem ao receptor
-
-    // digitalWrite(13, true); // Pisca o led built-in para mostrar que enviou dados
-    // digitalWrite(13, false);
-
-    size(1);
-    feed(2);
-    cut();
-    delay(tempo);
   }
 }
 
@@ -120,7 +132,7 @@ void begin() // Inicializa
 }
 
 void justification(int just) // Escolhe a posição em que o texto será impresso
-{                            // 0 = Esquerda | 1 = Meio | 2 = Direita
+{ // 0 = Esquerda | 1 = Meio | 2 = Direita
   Serial.write(27);
   Serial.write(97);
   Serial.write(just);
@@ -141,7 +153,7 @@ void cut() // Pula 4 linhas e corta o papel
 }
 
 void color(bool col) // Muda a cor do texto
-{                    // 0 = Preto | 1 = Branco
+{ // 0 = Preto | 1 = Branco
   Serial.write(29);
   Serial.write(66);
   Serial.write(col);
@@ -151,14 +163,14 @@ void size(int siz) // Muda o tamanho do caracter, possui 8 tamanhos de altura e 
 // Largura: 0, 16, 32, 48, 64, 80, 96, 112
 // Altura: 0, 1, 2, 3, 4, 5, 6, 7
 // Soma-se o número das medidas desejadas e substitui a variável "siz"
-{                  
+{
   Serial.write(29);
   Serial.write(33);
   Serial.write(siz);
 }
 
 void bold(bool bol) // Coloca o texto em negrito
-{                   // 0 = Desligado | 1 = Ligado
+{ // 0 = Desligado | 1 = Ligado
   Serial.write(27);
   Serial.write(69);
   Serial.write(bol);
